d9e5e9464608db384f91918c753343852a46d325
nlcexiyue <nlcexiyue@126.com>
2020-12-28 23:22:54 +0800

    oauth2.0

---
+++ distributed_security_uaa/src/main/java/com/qiangqiang/security/distributed/uaa/config/AuthorizationServer.java
@@ -0,0 +1,39 @@
+package com.qiangqiang.security.distributed.uaa.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@EnableAuthorizationServer  //标识为一个授权服务
+@Configuration
+public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+    public AuthorizationServer() {
+        super();
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+    }
+
+    //配置客户端详情服务
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //使用内存
+        clients.inMemory()
+                .withClient("c1")   //client_id
+        .secret(new BCryptPasswordEncoder().encode("secret"))   //客户端秘钥
+        .resourceIds("res1","res2")    //可以访问的资源列表
+        .authorizedGrantTypes(
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+    }
+}
